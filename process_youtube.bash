8a1abf310ecbddad5a4b4b028130f52c3886830a
myf <myf@worldtalks.org>
2016-09-21 20:07:12 -0400

    only pulling audio, and no ffmpeg transformation

--- process_youtube.bash
+++ process_youtube.bash
@@ -1,21 +1,7 @@
+# this is the version that only concernes audio
 id=$1
-video_file=buf/video_$1
 audio_file=buf/audio_$1
-audio_oga=$audio_file.oga
-
-tmp_file=$video_file.tmp
-tmp_file2=$tmp_file.simp
 tmp_audio=$audio_file.tmp
 
-#torsocks -i wget -O $video_file $video_url
-youtube-dl -f "worstvideo" $id -o "$video_file"
-ffprobe -show_frames -of compact=p=0 -f lavfi "movie=$video_file,select=gt(scene\,.35)" > $tmp_file
-cat $tmp_file |awk -F '|' '{print $5}' | awk -F '=' '{print $2}' > $tmp_file2
-rm $tmp_file
-
-#torsocks -i wget -O $audio_file $audio_url
 youtube-dl -f "bestaudio" $id -o "$audio_file"
-ffmpeg -i $audio_file $audio_oga
-aubiotrack $audio_oga > $tmp_audio
-rm $audio_file
-rm $audio_oga
+aubiotrack $audio_file > $tmp_audio
